69e9184d06a640a726b81c19b79e9013514aaf93
koibhinheen <koibhinheen@gmail.com>
2023-06-08 19:18:24 +0500

    Relational operators + compound assignment operators

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,4 +1,4 @@
-use std::{fs::read_to_string, ops::Add};
+use std::{fs::read_to_string};
 
 #[derive(Debug)]
 pub enum Token {
@@ -6,45 +6,65 @@ pub enum Token {
     Keyword(KeywordType),
     Literal(String),
     Op(OpType),
-    Punctuator(PunctuatorType),
     Invalid(char),
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 pub enum KeywordType {
     Fun,
     Return,
     Let,
 }
 
-#[derive(Debug)]
-pub enum PunctuatorType {
+#[derive(Debug, Clone, Copy)]
+pub enum OpType {
+    Add, Sub, Mul, Div, Mod,
+
+    LessThan,
+    GreaterThan,
+    LessThanEqualTo,
+    GreaterThanEqualTo,
+    EqualTo,
+    NotEqualTo,
+
+    Assign,
+    AddAssign,
+    SubAssign,
+    MulAssign,
+    DivAssign,
+    ModAssign,
+
     Comma,
     OpenCurlyBrace,
     CloseCurlyBrace,
     OpenParen,
-    CloseParen
+    CloseParen,
 }
 
-#[derive(Debug)]
-pub enum OpType {
-    Arithmatic(ArithmaticType),
-    Assign(AssignType),
-}
-
-#[derive(Debug)]
-pub enum AssignType {
-    Simple
-}
-
-#[derive(Debug)]
-pub enum ArithmaticType {
-    Add,
-    Sub,
-    Mul,
-    Div,
-    Mod
-}
+const OPERATOR_MAPPING: &[(&str, OpType)] = &[
+    ("==", OpType::EqualTo),
+    ("!=", OpType::NotEqualTo),
+    ("<=", OpType::LessThanEqualTo),
+    (">=", OpType::GreaterThanEqualTo),
+    ("+=", OpType::AddAssign),
+    ("-=", OpType::SubAssign),
+    ("/=", OpType::DivAssign),
+    ("*=", OpType::MulAssign),
+    ("%=", OpType::ModAssign),
+    ("<",  OpType::LessThan),
+    (">",  OpType::GreaterThan),
+    ("+",  OpType::Add),
+    ("-",  OpType::Sub),
+    ("/",  OpType::Div),
+    ("*",  OpType::Mul),
+    ("%",  OpType::Mod),
+    ("=",  OpType::Assign),
+    ("{",  OpType::OpenCurlyBrace),
+    ("}",  OpType::CloseCurlyBrace),
+    ("(",  OpType::OpenParen),
+    (")",  OpType::CloseParen),
+    (",",  OpType::Comma),
+];
 
 impl KeywordType {
     fn from_str(str: &str) -> Option<Self> {
@@ -73,10 +93,6 @@ impl Lexer {
         };
     }
 
-    fn is_punctuator(ch: char) -> bool {
-        return "{}(),".contains(ch);
-    }
-
     pub fn from_file(path: &str) -> Self {
         let contents = read_to_string(path).expect("Invalid path!");
         return Self::new(contents);
@@ -88,13 +104,6 @@ impl Lexer {
             return None;
         }
         return Some(self.contents[self.next_index]);
-    }
-
-    fn next_ch_eq(&self, ch: char) -> bool {
-        if let Some(peeked) = self.peek_ch() {
-            return peeked == ch;
-        }
-        return false;
     }
 
     fn next_ch(&mut self) -> Option<char> {
@@ -128,48 +137,31 @@ impl Lexer {
         }
     }
 
-    fn capture_punctuator(&mut self) -> Token {
-        use PunctuatorType::*;
-
-        let Some(ch) = self.next_ch() else {
-            panic!("Unexpected EOF!");
-        };
-
-        let punctuator_type = match ch {
-            '{' => OpenCurlyBrace,
-            '}' => CloseCurlyBrace,
-            '(' => OpenParen,
-            ')' => CloseParen,
-            ',' => Comma,
-            _ => panic!("Invalid punctuator token!")
-        };
-
-        return Token::Punctuator(punctuator_type);
+    fn capture(&mut self, str: &str) -> bool {
+        for (i, ch) in str.chars().enumerate() {
+            if i + self.next_index > self.contents.len() {
+                return false;
+            }
+            if self.contents[self.next_index + i] != ch {
+                return false;
+            }
+        }
+        self.next_index += str.len();
+        return true;
     }
 
-    fn is_operator(ch: char) -> bool {
-        return "+-*/%=".contains(ch);
+    fn capture_invalid(&mut self, ch: char) -> Token {
+        self.next_ch();
+        return Token::Invalid(ch);
     }
 
-    fn capture_operator(&mut self) -> Token {
-        use OpType::*;
-        use ArithmaticType::*;
-
-        let Some(ch) = self.next_ch() else {
-            panic!("Unexpected EOF!");
-        };
-
-        let op_type = match ch {
-            '+' => Arithmatic(Add),
-            '-' => Arithmatic(Sub),
-            '*' => Arithmatic(Mul),
-            '/' => Arithmatic(Div),
-            '%' => Arithmatic(Mod),
-            '=' => Assign(AssignType::Simple),
-            _ => panic!("Invalid operator token: {}", ch)
-        };
-
-        return Token::Op(op_type);
+    fn capture_operator(&mut self) -> Option<OpType> {
+        for (string, op_type) in OPERATOR_MAPPING.iter() {
+            if self.capture(string) {
+                return Some(*op_type);
+            }
+        }
+        return None;
     }
 }
 
@@ -182,37 +174,31 @@ impl Iterator for Lexer {
         self.skip_whitespace();
         let ch = self.peek_ch()?;
         let token = match ch {
-            ch if Self::is_punctuator(ch) => self.capture_punctuator(),
-            ch if Self::is_operator(ch) => self.capture_operator(),
+
+            ch if !ch.is_alphanumeric() => if let Some(op_type) = self.capture_operator() {
+                Op(op_type)
+            } else {
+                self.capture_invalid(ch)
+            }
             ch if ch.is_numeric() => {
-                let mut literal =
-                    String::new() + self.capture_while(|c| c.is_numeric()).as_str();
-                if self.next_ch_eq('.') {
-                    self.next_ch();
+                let mut literal = self.capture_while(|c| c.is_numeric());
+                if self.capture(".") {
                     literal += ".";
-                    literal += self.capture_while(|c| c.is_numeric()).as_str();
+                    literal += &self.capture_while(|c| c.is_numeric());
                 }
                 Literal(literal)
             }
             ch if ch.is_alphabetic() || ch == '_' => {
-                let mut ident = String::new();
-                while let Some(ch) = self.peek_ch() {
-                    if ch.is_alphanumeric() || ch == '_' {
-                        ident.push(self.next_ch().unwrap());
-                    } else {
-                        break;
-                    }
-                }
+                let ident = self.capture_while(|c| c.is_alphanumeric() || ch == '_');
 
-                if let Some(keyword_type) = KeywordType::from_str(ident.as_str()) {
+                if let Some(keyword_type) = KeywordType::from_str(&ident) {
                     Keyword(keyword_type)
                 } else {
                     Ident(ident)
                 }
             }
             _ => {
-                self.next_ch();
-                Token::Invalid(ch)
+                self.capture_invalid(ch)
             }
         };
         return Some(token);
